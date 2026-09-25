7c968183dea81cf32173d436108333e7ec29698a
Sander van der Burg <svanderburg@gmail.com>
2026-09-25 23:12:39 +0200

    Fix warning while trying to convert a null value into HTML entities

--- src/SBData/View/HTML/Field/filefield.php
+++ src/SBData/View/HTML/Field/filefield.php
@@ -11,13 +11,18 @@ use SBData\Model\Field\FileField;
 
 function displayFileField(FileField $field): void
 {
-	print(htmlentities($field->exportValue()));
+	$value = $field->exportValue();
+
+	if($value !== null)
+		print(htmlentities($value));
 }
 
 function displayEditableFileField(string $name, FileField $field): void
 {
+	$value = $field->exportValue();
+
 	?>
-	<input name="<?= $name ?>" type="file" value="<?= htmlentities($field->exportValue()) ?>">
+	<input name="<?= $name ?>" type="file" value="<?= ($value === null) ? "" : htmlentities($field->exportValue()) ?>">
 	<?php
 }
 
