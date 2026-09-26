e348b06cba899322d1371384c532413890596f91
Sander van der Burg <svanderburg@gmail.com>
2026-09-26 14:39:24 +0200

    Fix warning with null values

--- src/SBData/View/HTML/Field/generichiddenfield.php
+++ src/SBData/View/HTML/Field/generichiddenfield.php
@@ -11,12 +11,14 @@ use SBData\Model\Field\GenericHiddenField;
 
 function displayEditableGenericHiddenFieldRow(string $name, bool $inline, GenericHiddenField $field): void
 {
+	$value = $field->exportValue();
+
 	if($inline)
 		$tag = "span";
 	else
 		$tag = "div";
 	?>
-	<<?= $tag ?> style="display: none;"><input name="<?= $name ?>" type="hidden" value="<?= htmlentities($field->exportValue()) ?>"></<?= $tag ?>>
+	<<?= $tag ?> style="display: none;"><input name="<?= $name ?>" type="hidden" value="<?= ($value === null) ? "" : htmlentities($value) ?>"></<?= $tag ?>>
 	<?php
 }
 
